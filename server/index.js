2a8914f42bb50a0262ceb1f0b5f82360ecb1445d
kaushal bhattarai <bhattaraikaushal123@gmail.com>
2024-11-24 14:05:24 -0600

    Backend setup done with database connection

--- server/index.js
+++ server/index.js
@@ -11,14 +11,14 @@ app.use(cors());   // additional middlewares
 app.use(express.json({limit: '50mb' }))
 
 app.get('/', async (req, res) => {
-   res.send('Hello from DALL_E!') 
+   res.send('Hello from DALL-E!') 
 })
 
 
 const startServer = async () => {
     try{
         connectDB(process.env.MONGODB_URL);
-        app.listen(8000, () => console.log('Server running at port http://localhost.8000')) 
+        app.listen(8000, () => console.log('Server running at port http://localhost:8000')) 
     }catch (error){ 
         console.log(error);
     }
